f6bab66aaeff9de69e243efc5242d1b54433bbf1
groapaantonio <groapaantonio@gmail.com>
2025-08-05 12:50:40 +0100

    Add files via upload

---
+++ products.js
@@ -0,0 +1,59 @@
+// products.js
+const products = [
+  {
+    id: "carving-1",
+    name: "Mother and Baby",
+    price: 119.99,
+    description: "Beautiful wood carving of a mother holding her baby.",
+    image: "art1.jpg",
+  },
+  {
+    id: "carving-2",
+    name: "Crashing Waves",
+    price: 499.99,
+    description: "Wood carving of a large boat crashing against strong waves.",
+    image: "art2.jpg",
+  },
+  {
+    id: "carving-3",
+    name: "Man's Best Friend",
+    price: 479.99,
+    description: "Wood carving of a man playing the violin with his beloved companion.",
+    image: "art3.jpg",
+  },
+  {
+    id: "carving-5",
+    name: "Cottage",
+    price: 199.99,
+    description: "Wood carving of a humble cottage.",
+    image: "art5.jpg",
+  },
+  {
+    id: "carving-6",
+    name: "Praying Hands",
+    price: 349.99,
+    description: "Wood carving of hands praying.",
+    image: "art6.jpg",
+  },
+  {
+    id: "carving-7",
+    name: "Old Man",
+    price: 479.99,
+    description: "Wood carving of an old man sitting and thinking.",
+    image: "art7.jpg",
+  },
+  {
+    id: "carving-8",
+    name: "Stumbling Man",
+    price: 499.99,
+    description: "Wood carving of a man holding onto a post.",
+    image: "art8.jpg",
+  },
+  {
+    id: "carving-9",
+    name: "Yellow Bird",
+    price: 129.99,
+    description: "Wood painting of a yellow bird perched on a branch.",
+    image: "art9.jpg",
+  }
+];
